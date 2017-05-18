e633b4d8ab7cedfe28fb6e675db606afe239f986
Cominc <cominc@outlook.it>
2017-05-18 11:20:58 +0200

    Initial commit

---
+++ app/src/main/java/com/comincini_micheli/quest4run/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.comincini_micheli.quest4run;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
