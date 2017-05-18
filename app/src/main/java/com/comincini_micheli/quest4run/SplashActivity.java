871201c5ae7c96b953083eb05754489ce028b1ab
Cominc <cominc@outlook.it>
2017-05-18 13:42:48 +0200

    Creazione Splash screen

--- app/src/main/java/com/comincini_micheli/quest4run/SplashActivity.java
+++ app/src/main/java/com/comincini_micheli/quest4run/SplashActivity.java
@@ -1,13 +1,26 @@
 package com.comincini_micheli.quest4run;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    int TIMEOUT_SPLASH = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, TrainingActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIMEOUT_SPLASH);
     }
 }
