fac35523939232b01796e685426540e16856a1c5
giamma392 <giamma392@live.it>
2017-06-08 15:56:38 +0200

    uscita con click da about us activity

--- app/src/main/java/com/comincini_micheli/quest4run/activity/AboutUsActivity.java
+++ app/src/main/java/com/comincini_micheli/quest4run/activity/AboutUsActivity.java
@@ -2,6 +2,8 @@ package com.comincini_micheli.quest4run.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.comincini_micheli.quest4run.R;
 
@@ -11,5 +13,15 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.about_us_layout);
+        layout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
     }
+
 }
