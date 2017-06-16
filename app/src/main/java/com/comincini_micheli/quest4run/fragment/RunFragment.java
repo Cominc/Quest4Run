1d46a0b1a324c9de9010347e59ea3820752ec374
Cominc <cominc@outlook.it>
2017-06-16 21:32:12 +0200

    Prima implementazione GPS in fragment Run

--- app/src/main/java/com/comincini_micheli/quest4run/fragment/RunFragment.java
+++ app/src/main/java/com/comincini_micheli/quest4run/fragment/RunFragment.java
@@ -2,6 +2,7 @@ package com.comincini_micheli.quest4run.fragment;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -36,11 +37,41 @@ public class RunFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button btnGPS = (Button) getView().findViewById(R.id.buttonGPS);
+        final TextView textViewLocation = (TextView) getView().findViewById(R.id.textview_location);
+
+        LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                Toast.makeText(getContext(), "Location changed!", Toast.LENGTH_SHORT).show();
+                textViewLocation.setText("Latitudine: "+ location.getLatitude()+"\n"+"Longitudine: "+location.getLongitude()+"\n"+"Altitudine: "+location.getAltitude()+"\n");
+                location.getAltitude();
+                //makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "Permessi GPS mancanti", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+
+        Button btnGPS = (Button) getView().findViewById(R.id.button_gps);
         btnGPS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView t = (TextView) getActivity().findViewById(R.id.coordinates);
+                TextView t = (TextView) getActivity().findViewById(R.id.textview_location);
                 t.setText("ciao");
             }
         });
