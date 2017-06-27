3371ff31e42687b48fd68e1ef87be00988618f1f
Cominc <cominc@outlook.it>
2017-06-27 09:55:14 +0200

    Aggiunta notifica con info quando si conclude allenamento, corretta lettura valori ricompense, aggiunto controllo distanza percorsa maggiore di zero prima di controllare se i task sono stati completati

--- app/src/main/java/com/comincini_micheli/quest4run/fragment/RunFragment.java
+++ app/src/main/java/com/comincini_micheli/quest4run/fragment/RunFragment.java
@@ -1,7 +1,11 @@
 package com.comincini_micheli.quest4run.fragment;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.DatabaseErrorHandler;
@@ -21,6 +25,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.comincini_micheli.quest4run.R;
+import com.comincini_micheli.quest4run.activity.TaskHistoryActivity;
 import com.comincini_micheli.quest4run.objects.Character;
 import com.comincini_micheli.quest4run.objects.Gps;
 import com.comincini_micheli.quest4run.objects.Task;
@@ -185,6 +190,11 @@ public class RunFragment extends Fragment {
             public void onClick(View view) {
                 Toast.makeText( getContext(),"Fine attività",Toast.LENGTH_SHORT).show();
                 active = false;
+                locationManager.removeUpdates(locationListener);
+                btnGPS_stop.setVisibility(View.GONE);
+                btnGPS_start.setVisibility(View.VISIBLE);
+                if (previusLocation != null)
+                    mMapGoogle.addMarker(new MarkerOptions().position(new LatLng(previusLocation.getLatitude(), previusLocation.getLongitude())).anchor(0.0f, 1.0f).icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_finish)));
                 Toast.makeText( getContext(),"GPS points: "+db.getGpsCount(),Toast.LENGTH_SHORT).show();
                 Toast.makeText( getContext(),"Distanza (m): "+totalDistance,Toast.LENGTH_SHORT).show();
 
@@ -192,105 +202,124 @@ public class RunFragment extends Fragment {
                 long totalTime = (finishTime - startTime)/ Constants.MILLISECONDS_A_SECOND;
                 long numberMinutes = totalTime / Constants.SECONDS_A_MINUTE;
                 int totalRewardEarned = 0;
-                if(numberMinutes > 0)
-                {
-                    Toast.makeText( getContext(),"Durata : "+numberMinutes*Constants.SECONDS_A_MINUTE + " minuti e " +
-                                    (totalTime - numberMinutes * Constants.SECONDS_A_MINUTE) + " secondi",Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText( getContext(),"Durata (s): "+totalTime,Toast.LENGTH_SHORT).show();
-                }
-
-                locationManager.removeUpdates(locationListener);
-                btnGPS_stop.setVisibility(View.GONE);
-                btnGPS_start.setVisibility(View.VISIBLE);
-
-                //DISTANCE
-                List<Task> tasks_distance;
-                tasks_distance = db.getTasks(false,true, Constants.DISTANCE_TYPE_TASK);
-                for(int i=0; i<tasks_distance.size(); i++){
-                    String s = getActivity().getResources().getStringArray(R.array.task_distance_goal)[Integer.parseInt(tasks_distance.get(i).getGoal())].toString();
-                    double goalValue = Double.parseDouble(s.substring(0, s.length() - 3))*Constants.FROM_KM_TO_M;
-                    if((tasks_distance.get(i).getProgress()+totalDistance) > goalValue){
-                        tasks_distance.get(i).setCompleted(true);
-                        totalRewardEarned += tasks_distance.get(i).getReward();
-                    }
-                    else
-                        tasks_distance.get(i).setProgress(tasks_distance.get(i).getProgress()+totalDistance);
-
-                    tasks_distance.get(i).setExecDate(finishTime);
-                    db.updateTask(tasks_distance.get(i));
-                }
-
-                //RITHM
-                List<Task> tasks_rithm;
-                tasks_rithm = db.getTasks(false,true, Constants.PACE_TYPE_TASK);
-                for(int i=0; i<tasks_rithm.size(); i++){
-                    String s = getActivity().getResources().getStringArray(R.array.task_rithm_goal)[Integer.parseInt(tasks_rithm.get(i).getGoal())].toString();
-                    double goalValue = Double.parseDouble(s.substring(0, s.length() - 4));
-                    if(goalValue <= totalSpeed/totalGPSPoints)
-                    {
-                        tasks_rithm.get(i).setCompleted(true);
-                        tasks_rithm.get(i).setExecDate(finishTime);
-                        totalRewardEarned += tasks_rithm.get(i).getReward();
+                int totalNewTaskCompleted = 0;
+                if(totalDistance > 0) {
+                    if (numberMinutes > 0) {
+                        Toast.makeText(getContext(), "Durata : " + numberMinutes * Constants.SECONDS_A_MINUTE + " minuti e " +
+                                (totalTime - numberMinutes * Constants.SECONDS_A_MINUTE) + " secondi", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getContext(), "Durata (s): " + totalTime, Toast.LENGTH_SHORT).show();
                     }
 
-                    db.updateTask(tasks_rithm.get(i));
+                    //DISTANCE
+                    List<Task> tasks_distance;
+                    tasks_distance = db.getTasks(false, true, Constants.DISTANCE_TYPE_TASK);
+                    for (int i = 0; i < tasks_distance.size(); i++) {
+                        String s = getActivity().getResources().getStringArray(R.array.task_distance_goal)[Integer.parseInt(tasks_distance.get(i).getGoal())];
+                        double goalValue = Double.parseDouble(s.substring(0, s.length() - 3)) * Constants.FROM_KM_TO_M;
+                        if ((tasks_distance.get(i).getProgress() + totalDistance) > goalValue) {
+                            tasks_distance.get(i).setCompleted(true);
+                            String rewardString = getActivity().getResources().getStringArray(R.array.task_distance_reward)[tasks_distance.get(i).getReward()];
+                            int rewardValue = Integer.parseInt(rewardString.substring(0, rewardString.length() - 2));
+                            totalRewardEarned += rewardValue;
+                            totalNewTaskCompleted++;
+                        } else
+                            tasks_distance.get(i).setProgress(tasks_distance.get(i).getProgress() + totalDistance);
+
+                        tasks_distance.get(i).setExecDate(finishTime);
+                        db.updateTask(tasks_distance.get(i));
+                    }
+
+                    //RITHM
+                    List<Task> tasks_rithm;
+                    tasks_rithm = db.getTasks(false, true, Constants.PACE_TYPE_TASK);
+                    for (int i = 0; i < tasks_rithm.size(); i++) {
+                        String s = getActivity().getResources().getStringArray(R.array.task_rithm_goal)[Integer.parseInt(tasks_rithm.get(i).getGoal())];
+                        double goalValue = Double.parseDouble(s.substring(0, s.length() - 4));
+                        if (goalValue <= totalSpeed / totalGPSPoints) {
+                            tasks_rithm.get(i).setCompleted(true);
+                            tasks_rithm.get(i).setExecDate(finishTime);
+                            String rewardString = getActivity().getResources().getStringArray(R.array.task_rithm_reward)[tasks_rithm.get(i).getReward()];
+                            int rewardValue = Integer.parseInt(rewardString.substring(0, rewardString.length() - 2));
+                            totalRewardEarned += rewardValue;
+                            totalNewTaskCompleted++;
+                        }
+
+                        db.updateTask(tasks_rithm.get(i));
+                    }
+
+                    //CONSTANCE
+                    List<Task> tasks_constance;
+                    tasks_constance = db.getTasks(false, true, Constants.CONSTANCE_TYPE_TASK);
+                    for (int i = 0; i < tasks_constance.size(); i++) {
+                        String s = getActivity().getResources().getStringArray(R.array.task_constance_goal)[Integer.parseInt(tasks_constance.get(i).getGoal())];
+                        int goalValue = Integer.parseInt(s.substring(0, s.length() - 7));
+
+                        Date lastExecDate = new Date(tasks_constance.get(i).getExecDate());
+                        Date yesterdayDate = new Date(finishTime - Constants.MILLISECONDS_A_DAY);
+
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                        //TODO if ristrutturati controllare se corretto
+                        if (sdf.format(yesterdayDate).equals(sdf.format(lastExecDate))) {
+                            int progress = (int) tasks_constance.get(i).getProgress() + 1;
+                            tasks_constance.get(i).setProgress(progress);
+                        } else {
+                            tasks_constance.get(i).setProgress(1);
+                        }
+                        if (tasks_constance.get(i).getProgress() >= goalValue) {
+                            tasks_constance.get(i).setCompleted(true);
+                            String rewardString = getActivity().getResources().getStringArray(R.array.task_constance_reward)[tasks_constance.get(i).getReward()];
+                            int rewardValue = Integer.parseInt(rewardString.substring(0, rewardString.length() - 2));
+                            totalRewardEarned += rewardValue;
+                            totalNewTaskCompleted++;
+                        }
+                        tasks_constance.get(i).setExecDate(finishTime);
+                        db.updateTask(tasks_constance.get(i));
+                    }
+
+                    List<Task> tasks_duration;
+                    tasks_duration = db.getTasks(false, true, Constants.DURATION_TYPE_TASK);
+                    for (int i = 0; i < tasks_duration.size(); i++) {
+                        String s = getActivity().getResources().getStringArray(R.array.task_duration_goal)[Integer.parseInt(tasks_duration.get(i).getGoal())];
+                        int goalValue = Integer.parseInt(s.substring(0, s.length() - 7)) * Constants.SECONDS_A_MINUTE;
+                        if (goalValue <= totalTime) {
+                            tasks_duration.get(i).setCompleted(true);
+                            tasks_duration.get(i).setExecDate(finishTime);
+                            String rewardString = getActivity().getResources().getStringArray(R.array.task_duration_reward)[tasks_duration.get(i).getReward()];
+                            int rewardValue = Integer.parseInt(rewardString.substring(0, rewardString.length() - 2));
+                            totalRewardEarned += rewardValue;
+                            totalNewTaskCompleted++;
+                        }
+
+                        db.updateTask(tasks_duration.get(i));
+                    }
+
+                    //TODO non da errore sia con getActivity sia con getContext ???
+                    SharedPreferences firstLaunchSetting = getActivity().getSharedPreferences(Constants.NAME_PREFS, Context.MODE_PRIVATE);
+                    int characterId = firstLaunchSetting.getInt(Constants.CHAR_ID_PREFERENCE, -1);
+                    Character myCharacter = db.getCharacter(characterId);
+                    myCharacter.setWallet(myCharacter.getWallet() + totalRewardEarned);
+                    db.updateCharacter(myCharacter);
+                    Toast.makeText(getContext(), "Monete guadagnate: " + totalRewardEarned + getResources().getString(R.string.cents_symbol_label), Toast.LENGTH_SHORT).show();
+
+                    if (totalNewTaskCompleted > 0) {
+                        NotificationManager nm = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                        Intent notifyIntent = new Intent(getActivity(), TaskHistoryActivity.class);
+                        PendingIntent intent = PendingIntent.getActivity(getActivity(), 0, notifyIntent, 0);
+                        final Notification notifyDetails = new Notification.Builder(getContext())
+                                .setTicker("Ticker")
+                                //.setSmallIcon(R.mipmap.ic_test)
+                                .setSmallIcon(R.drawable.splash)
+                                .setWhen(System.currentTimeMillis())
+                                .setContentText("Hai guadagnato " + totalRewardEarned + getResources().getString(R.string.cents_symbol_label))
+                                .setContentTitle("Hai completato " + totalNewTaskCompleted + " task!")
+                                .setContentIntent(intent)
+                                .build();
+                        notifyDetails.flags = notifyDetails.flags | Notification.FLAG_AUTO_CANCEL;
+                        //TODO 1 = SIMPLE_NOTIFICATION_ID
+                        nm.notify(1, notifyDetails);
+                    }
                 }
-
-                //CONSTANCE
-                List<Task> tasks_constance;
-                tasks_constance = db.getTasks(false,true, Constants.CONSTANCE_TYPE_TASK);
-                for(int i=0; i<tasks_constance.size(); i++){
-                    String s = getActivity().getResources().getStringArray(R.array.task_constance_goal)[Integer.parseInt(tasks_constance.get(i).getGoal())].toString();
-                    int goalValue = Integer.parseInt(s.substring(0, s.length() - 7));
-
-                    Date lastExecDate = new Date(tasks_constance.get(i).getExecDate());
-                    Date yesterdayDate = new Date(finishTime - Constants.MILLISECONDS_A_DAY);
-
-                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-                    //TODO if ristrutturati controllare se corretto
-                    if(sdf.format(yesterdayDate).equals(sdf.format(lastExecDate)))
-                    {
-                        int progress = (int) tasks_constance.get(i).getProgress() + 1;
-                        tasks_constance.get(i).setProgress(progress);
-                    }
-                    else
-                    {
-                        tasks_constance.get(i).setProgress(1);
-                    }
-                    if(tasks_constance.get(i).getProgress() >= goalValue){
-                        tasks_constance.get(i).setCompleted(true);
-                        totalRewardEarned += tasks_constance.get(i).getReward();
-                    }
-                    tasks_constance.get(i).setExecDate(finishTime);
-                    db.updateTask(tasks_constance.get(i));
-                }
-
-                List<Task> tasks_duration;
-                tasks_duration = db.getTasks(false,true, Constants.DURATION_TYPE_TASK);
-                for(int i=0; i<tasks_duration.size(); i++){
-                    String s = getActivity().getResources().getStringArray(R.array.task_duration_goal)[Integer.parseInt(tasks_duration.get(i).getGoal())].toString();
-                    int goalValue = Integer.parseInt(s.substring(0, s.length() - 7))*Constants.SECONDS_A_MINUTE;
-                    if(goalValue <= totalTime)
-                    {
-                        tasks_duration.get(i).setCompleted(true);
-                        tasks_duration.get(i).setExecDate(finishTime);
-                        totalRewardEarned += tasks_duration.get(i).getReward();
-                    }
-
-                    db.updateTask(tasks_duration.get(i));
-                }
-                if(previusLocation!=null)
-                    mMapGoogle.addMarker(new MarkerOptions().position(new LatLng(previusLocation.getLatitude(),previusLocation.getLongitude())).anchor(0.0f, 1.0f).icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_finish)));
-                //TODO non da errore sia con getActivity sia con getContext ???
-                SharedPreferences firstLaunchSetting = getActivity().getSharedPreferences(Constants.NAME_PREFS, Context.MODE_PRIVATE);
-                int characterId = firstLaunchSetting.getInt(Constants.CHAR_ID_PREFERENCE, -1);
-                Character myCharacter = db.getCharacter(characterId);
-                myCharacter.setWallet(myCharacter.getWallet()+totalRewardEarned);
-                db.updateCharacter(myCharacter);
-                Toast.makeText( getContext(),"Monete guadagnate: "+totalRewardEarned+getResources().getString(R.string.cents_symbol_label),Toast.LENGTH_SHORT).show();
             }
         });
     }
