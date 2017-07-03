014bc5be79c46d839d5667599398effbf5492fa4
Cominc <cominc@outlook.it>
2017-07-03 17:26:13 +0200

    Aggiunto controllo per evitare crash nel caso in cui non ci sia una quest attiva

--- app/src/main/java/com/comincini_micheli/quest4run/other/AlarmNotificationReceiver.java
+++ app/src/main/java/com/comincini_micheli/quest4run/other/AlarmNotificationReceiver.java
@@ -26,39 +26,40 @@ public class AlarmNotificationReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         DatabaseHandler db = new DatabaseHandler(context);
         Quest activeQuest = db.getActiveQuest();
+        //TODO guarda file error.txt, if aggiunto per evitare crash
+        if(activeQuest!=null) {
+            Intent notifyIntent = new Intent(context, QuestHistoryActivity.class);
+            PendingIntent myIntent = PendingIntent.getActivity(context, 0, notifyIntent, 0);
 
-        Intent notifyIntent = new Intent(context, QuestHistoryActivity.class);
-        PendingIntent myIntent = PendingIntent.getActivity(context, 0, notifyIntent, 0);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-
-        builder.setAutoCancel(true)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(context.getResources().getString(R.string.quest_complete_notification_title))
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(
-                        String.format(context.getResources().getString(R.string.quest_complete_notification_text),
-                                activeQuest.getTitle(),
-                                activeQuest.getExpReward())))
-                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
-                .setContentInfo("Info")
-                .setContentIntent(myIntent);
+            builder.setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(context.getResources().getString(R.string.quest_complete_notification_title))
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(
+                            String.format(context.getResources().getString(R.string.quest_complete_notification_text),
+                                    activeQuest.getTitle(),
+                                    activeQuest.getExpReward())))
+                    .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                    .setContentInfo("Info")
+                    .setContentIntent(myIntent);
 
 
-        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(1,builder.build());
-        activeQuest.setCompleted(true);
-        activeQuest.setActive(false);
-        activeQuest.setDateFinish(System.currentTimeMillis());
-        db.updateQuest(activeQuest);
+            notificationManager.notify(1, builder.build());
+            activeQuest.setCompleted(true);
+            activeQuest.setActive(false);
+            activeQuest.setDateFinish(System.currentTimeMillis());
+            db.updateQuest(activeQuest);
 
-        SharedPreferences settings = context.getSharedPreferences(Constants.NAME_PREFS, Context.MODE_PRIVATE);
-        Character myCharacter = db.getCharacter(settings.getInt(Constants.CHAR_ID_PREFERENCE,-1));
-        myCharacter.setExp(myCharacter.getExp()+activeQuest.getExpReward());
-        db.updateCharacter(myCharacter);
-
+            SharedPreferences settings = context.getSharedPreferences(Constants.NAME_PREFS, Context.MODE_PRIVATE);
+            Character myCharacter = db.getCharacter(settings.getInt(Constants.CHAR_ID_PREFERENCE, -1));
+            myCharacter.setExp(myCharacter.getExp() + activeQuest.getExpReward());
+            db.updateCharacter(myCharacter);
+        }
         //TODO gestire refresh QuestFragment list
         //TODO gestire back quando non esiste activity main
     }
